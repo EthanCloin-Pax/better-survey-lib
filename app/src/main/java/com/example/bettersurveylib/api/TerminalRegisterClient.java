77541418eeeef13d272d024e0c3ae507206ee77b
Ethan Cloin <ethan.cloin@pax.us>
2023-03-10 12:51:03 -0500

    Add TerminalRegister client definition

--- app/src/main/java/com/example/bettersurveylib/api/TerminalRegisterClient.java
+++ app/src/main/java/com/example/bettersurveylib/api/TerminalRegisterClient.java
@@ -1,2 +1,27 @@
-package com.example.bettersurveylib.api;public class TerminalRegisterClient {
+package com.example.bettersurveylib.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class TerminalRegisterClient {
+    private static final String BASE_URL= "https://dev.seamlesscommerce.com/services";
+    private static Retrofit retrofit = null;
+
+    static Retrofit getClient() {
+        // create interceptor to log request body
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        // create retrofit client
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
 }
